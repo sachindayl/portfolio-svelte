a07e6eff36b061b8d8df2afd83f5cf1de2b7ce81
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2023-02-09 14:47:51 -0500

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+@
+svelte.config.js,9/4/9412e23d6c9b081f2298258aff5115cc43e3308e
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl font-bold underline text-indigo-600">
+  Hello world!
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.500);
+  }
+</style>
