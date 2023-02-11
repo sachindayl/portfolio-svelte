256c41ffb14dd0166f1e0d10948b6235a3f66895
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2023-02-11 16:06:12 -0500

    hero setup

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,6 @@
+<div class="flex h-20 items-center invisible md:visible">
+	<div class="text-3xl grow pl-16">sachinda.liyanaarachchi</div>
+	<div class="px-4">Projects</div>
+	<div class="px-4">About</div>
+	<div class="pl-4 pr-16">Contact</div>
+</div>
