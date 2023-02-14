26d1d900cd84a63cf4abc55a19bec4a01b9b8aa2
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2023-02-14 12:23:11 -0500

    git add .

---
+++ src/components/ProjectCard.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import phone from '$lib/assets/images/22-removebg-preview.png';
+	import type { ProjectI } from '../models/ProjectI';
+	export let project: ProjectI;
+	export let isReverse: boolean;
+</script>
+
+<div class="flex  items-center px-16">
+	{#if isReverse}
+		<div class="flex-1 flex flex-col">
+			<div class="text-5xl">{project.name}</div>
+			<div class="text-xl py-8 font-light">
+				{project.description}
+			</div>
+			<div class="font-light">
+				Built with:
+				{#each project.tools as tool}
+					<span class="font-semibold">
+						{tool}
+					</span>
+				{/each}
+			</div>
+		</div>
+	{/if}
+	<div class="flex-1 w-full flex {isReverse ? 'justify-end' : 'justify-start'}">
+		<div class="rounded-2xl bg-teal-600 w-96 h-96 p-4">
+			<img class="object-contain w-full h-full" src={phone} alt="phone" />
+		</div>
+	</div>
+	{#if !isReverse}
+		<div class="flex-1 flex flex-col items-end">
+			<div class="text-5xl">{project.name}</div>
+			<div class="text-xl py-8 font-light">
+				{project.description}
+			</div>
+			<div class="font-light">
+				Built with:
+				{#each project.tools as tool}
+					<span class="font-semibold">
+						{tool}
+					</span>
+				{/each}
+			</div>
+		</div>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
-<script>
+<script lang="ts">
 	import Hero from '../views/Hero.svelte';
-	import NavBar from "../components/NavBar.svelte";
+	import NavBar from '../components/NavBar.svelte';
+	import Projects from '../views/Projects.svelte';
+	import type {PageServerData} from "../../.svelte-kit/types/src/routes/$types";
+
+	export let data: PageServerData;
 </script>
 
 <div class="h-screen">
 	<NavBar />
 	<Hero />
+	<Projects projects={data.projects}/>
 </div>
