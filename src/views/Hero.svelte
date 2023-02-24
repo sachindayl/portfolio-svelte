4cea2e692208cf667059ed4bf5b88667a3a40cc5
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2023-02-24 10:15:14 -0500

    hero design changes to support tablets

--- src/views/Hero.svelte
+++ src/views/Hero.svelte
@@ -1,16 +1,24 @@
 <script>
 	import myImage from '$lib/assets/images/me.png?h=360&w=720&webp';
-	import { scrollRef } from 'svelte-scrolling'
+	import { scrollRef } from 'svelte-scrolling';
 </script>
 
-<div class="md:h-2/3 px-0 md:px-8 bg-gradient-to-l from-teal-200">
-	<div use:scrollRef={'hero'} class="container mx-auto flex flex-col md:flex-row items-center px-0 md:px-16 md:h-full w-full">
-		<div class="animate__animated animate__fadeInLeft flex-1 flex-col text-center md:text-start p-8 md:p-0 w-full">
-			<div class="text-5xl md:7xl">
+<div class="md:h-5/6 lg:h-2/3 px-0 md:px-8 bg-gradient-to-l from-teal-200">
+	<div
+		use:scrollRef={'hero'}
+		class="container mx-auto flex flex-col lg:flex-row items-center px-0 md:px-16 md:h-full w-full"
+	>
+		<div
+			class="animate__animated animate__fadeInLeft flex-1 flex-col text-center lg:text-start p-8 md:pt-24 md:pb-16 lg:p-0 w-full"
+		>
+			<div class="text-5xl lg:text-7xl">
 				Making the impossible possible with <span class="font-bold">Sachi:</span>
 			</div>
-			<div class="text-2xl md:text-4xl pt-4 font-light">Mobile and web development at its finest.</div>
-			<div class="flex flex-row justify-center md:justify-start space-x-2 py-4 md:py-8">
+			<div class="text-2xl md:text-4xl pt-4 font-light">
+				Mobile and web development at its finest.
+			</div>
+			<div class="flex flex-row justify-center lg:justify-start space-x-2 py-4 md:py-8">
+				<div class="text-small bg-teal-500 p-0.5 px-4 rounded-full">sachinda.yl@gmail.com</div>
 				<a href="https://www.github.com/sachindayl" target="_blank" rel="noreferrer">
 					<img
 						class="object-contain w-8 h-8 bg-teal-500 rounded-full p-0.5"
@@ -39,11 +47,21 @@
 				alt="myself"
 			/>
 		</div>
-		<div class="animate__animated animate__fadeInRight flex-none flex  flex-col w-full md:w-20 mt-20 mb-8 md:mb-0 md:mt-0 items-center md:items-end">
-			<div class="text-2xl md:text-4xl text-center md:text-end font-semibold">6+</div>
-			<div class="text-xl md:text-2xl text-center md:text-end font-light">Years of experience</div>
-			<div class="text-2xl md:text-4xl text-center md:text-end pt-8 md:pt-16 font-semibold">20+</div>
-			<div class="text-xl md:text-2xl text-center md:text-end font-light">Projects done</div>
+		<div
+			class="animate__animated animate__fadeInRight flex-none flex flex-col md:flex-row lg:flex-col md:justify-center w-full md:w-20 mt-20 mb-8 lg:mb-0 lg:mt-0 items-center lg:items-end"
+		>
+			<div class="px-4">
+				<div class="text-2xl lg:text-4xl text-center lg:text-end font-semibold">6+</div>
+				<div class="text-xl lg:text-2xl text-center lg:text-end font-light">
+					Years of experience
+				</div>
+			</div>
+			<div class="px-4">
+				<div class="text-2xl lg:text-4xl text-center lg:text-end pt-8 md:pt-0 lg:pt-16 font-semibold">
+					20+
+				</div>
+				<div class="text-xl lg:text-2xl text-center lg:text-end font-light">Projects done</div>
+			</div>
 		</div>
 	</div>
 </div>
