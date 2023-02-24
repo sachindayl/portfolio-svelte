290f9c6d1eed541b906257ed62fa778d33350776
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2023-02-24 09:46:22 -0500

    adding animations to the hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,10 @@
 	import type { PageServerData } from '../../.svelte-kit/types/src/routes/$types';
 	import Tools from '../views/Tools.svelte';
 	import Resume from '../views/Resume.svelte';
-	import type {ExperienceI} from "../models/ExperienceI";
+	import type { ExperienceI } from '../models/ExperienceI';
 
 	export let data: PageServerData;
-	let experienceData: [ExperienceI] = data.resume as [ExperienceI]
+	let experienceData: [ExperienceI] = data.resume as [ExperienceI];
 </script>
 
 <svelte:head>
@@ -17,11 +17,15 @@
 		name="description"
 		content="Crafting Digital Solutions for Mobile and Web: Explore the Portfolio of a Skilled Developer based in Toronto. I work on iOS, Android and Flutter projects."
 	/>
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+	/>
 </svelte:head>
 <div class="h-screen">
 	<NavBar />
 	<Hero />
 	<Projects projects={data.projects} />
 	<Tools toolsList={data.tools} />
-	<Resume resumeItemsList={experienceData}/>
+	<Resume resumeItemsList={experienceData} />
 </div>
--- src/views/Hero.svelte
+++ src/views/Hero.svelte
@@ -4,7 +4,7 @@
 
 <div class="md:h-2/3 px-0 md:px-8 bg-gradient-to-l from-teal-200">
 	<div class="container mx-auto flex flex-col md:flex-row items-center px-0 md:px-16 md:h-full w-full">
-		<div class="flex-1 flex-col text-center md:text-start p-8 md:p-0 w-full">
+		<div class="animate__animated animate__fadeInLeft flex-1 flex-col text-center md:text-start p-8 md:p-0 w-full">
 			<div class="text-5xl md:7xl">
 				Making the impossible possible with <span class="font-bold">Sachi:</span>
 			</div>
@@ -26,7 +26,7 @@
 				</a>
 			</div>
 		</div>
-		<div class="flex-1 relative w-full p-16 md:p-8">
+		<div class="animate__animated animate__zoomIn flex-1 relative w-full p-16 md:p-8">
 			<img
 				src="assets/images/blob.svg"
 				class="w-64 md:w-5/6 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
@@ -38,7 +38,7 @@
 				alt="myself"
 			/>
 		</div>
-		<div class="flex-none flex  flex-col w-full md:w-20 mt-20 mb-8 md:mb-0 md:mt-0 items-center md:items-end">
+		<div class="animate__animated animate__fadeInRight flex-none flex  flex-col w-full md:w-20 mt-20 mb-8 md:mb-0 md:mt-0 items-center md:items-end">
 			<div class="text-2xl md:text-4xl text-center md:text-end font-semibold">6+</div>
 			<div class="text-xl md:text-2xl text-center md:text-end font-light">Years of experience</div>
 			<div class="text-2xl md:text-4xl text-center md:text-end pt-8 md:pt-16 font-semibold">20+</div>
