927031f4af0c87633eafee9c5f7317b33ff9524f
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2023-11-14 09:18:45 -0500

    disable packages

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -5,7 +5,7 @@
 
 <div class="fixed flex h-20 items-center invisible md:visible bg-white dark:bg-black w-full shadow-lg z-50">
 	<a use:scrollTo={{ ref: 'hero', offset: -100 }} href="/#" class="text-3xl grow pl-16 text-black dark:text-white">sachinda.liyanaarachchi</a>
-	<a href="/packages" class="px-4 text-black dark:text-white">Packages</a>
+<!--	<a href="/packages" class="px-4 text-black dark:text-white">Packages</a>-->
 	<a use:scrollTo={{ ref: 'projects', offset: -100 }} href="/#" class="px-4 text-black dark:text-white">Projects</a>
 	<a use:scrollTo={{ ref: 'tools', offset: -100 }} href="/#" class="px-4 text-black dark:text-white">Tools</a>
 	<a use:scrollTo={{ ref: 'resume', offset: -100 }} href="/#" class="pl-4 pr-16 text-black dark:text-white">Resume</a>
--- src/components/NavBarOther.svelte
+++ src/components/NavBarOther.svelte
@@ -4,7 +4,7 @@
 
 <div class="fixed flex h-20 items-center invisible md:visible bg-white dark:bg-black w-full shadow-lg z-50">
 	<a href="/#" class="text-3xl grow pl-16 text-black dark:text-white">sachinda.liyanaarachchi</a>
-	<a href="/packages" class="px-4 text-black dark:text-white">Packages</a>
+<!--	<a href="/packages" class="px-4 text-black dark:text-white">Packages</a>-->
 	<a href="/#" class="px-4 text-black dark:text-white">Projects</a>
 	<a href="/#" class="px-4 text-black dark:text-white">Tools</a>
 	<a href="/#" class="pl-4 pr-16 text-black dark:text-white">Resume</a>
